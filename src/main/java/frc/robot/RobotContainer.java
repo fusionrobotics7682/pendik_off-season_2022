390e2734cde779b87306c64926879022c982ac40
okcu73 <alptng72@gmail.com>
2022-09-24 22:59:59 +0300

    Production Robot Container for TeleOp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.unit_transaction.shooter_cmd.tele_op.ShootInTarmacCmd;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.unit_transaction.chassis_cmd.tele_op.ArcadeDriveCmd;
+import frc.robot.commands.unit_transaction.feeder_cmd.tele_op.GetInFeederCmd;
+import frc.robot.commands.unit_transaction.feeder_cmd.tele_op.GetOutFeederCmd;
+import frc.robot.commands.unit_transaction.intake_cmd.tele_op.GetIntakeCmd;
+import frc.robot.commands.unit_transaction.intake_cmd.tele_op.GetOuttakeCmd;
+import frc.robot.commands.unit_transaction.shooter_cmd.tele_op.ShootTarmacCloserTeleCmd;
+import frc.robot.constants.JoystickIOConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 /**
@@ -17,12 +30,25 @@ import frc.robot.subsystems.ShooterSubsystem;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  ShooterSubsystem subsystem = new ShooterSubsystem();
+
+  // Cameras and joysticks defined here
+  Joystick joystick = JoystickIOConstants.getJoystick();
+  UsbCamera frontCamera;
+  UsbCamera rearCamera;
+
   // The robot's subsystems and commands are defined here...
-  ShootInTarmacCmd cmd = new ShootInTarmacCmd(subsystem, 10);
+  DriveSubsystem driveSubsystem = new DriveSubsystem();
+  IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  FeederSubsystem feederSubsystem = new FeederSubsystem();
+  ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem));
+    frontCamera = CameraServer.startAutomaticCapture(0);
+    rearCamera = CameraServer.startAutomaticCapture(1);
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -33,7 +59,13 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(joystick, 1).whileActiveContinuous(new GetIntakeCmd(intakeSubsystem));
+    new JoystickButton(joystick, 4).whileActiveContinuous(new GetOuttakeCmd(intakeSubsystem));
+    new JoystickButton(joystick, 2).whileActiveContinuous(new GetInFeederCmd(feederSubsystem));
+    new JoystickButton(joystick, 3).whileActiveContinuous(new GetOutFeederCmd(feederSubsystem));
+    new JoystickButton(joystick, 5).whileActiveContinuous(new ShootTarmacCloserTeleCmd(shooterSubsystem, 20));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
